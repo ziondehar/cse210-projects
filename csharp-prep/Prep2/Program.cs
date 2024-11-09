b4809a7979dac434ef5d3517b7d6cb991233e2a2
ziondehar <ziondehar@gmail.com>
2024-11-09 13:17:29 +0800

    updated prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -29,7 +29,7 @@ class Program
             }
         else 
             {
-                Console.WriteLine("This grade isnt possible");
+                Console.WriteLine("This isnt a possible grade");
             }
     }
 }
