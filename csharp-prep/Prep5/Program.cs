ffe747d1624ec770b46fe22d8e2d028ab34d34d0
ziondehar <ziondehar@gmail.com>
2024-11-09 18:09:38 +0800

    Proj 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep5 World!");
+        welcomeMessage();
+
+        string username = name();
+        int usernum = number();
+
+        int squaredNumber = square( usernum);
+
+        final (username,squaredNumber);
     }
+
+
+
+
+    static void welcomeMessage()
+        {
+        Console.WriteLine("Hello and Welcome!");
+        }
+
+    static string name()
+        {
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+            return name;
+                
+        }
+    static int number()
+        {
+            Console.WriteLine("what is your age");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+    static int square(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }    
+    static void final(string Name, int squaredNumber)
+        {
+            Console.Write($"{Name}, your squared number is {squaredNumber}");
+
+        }
+    
 }
