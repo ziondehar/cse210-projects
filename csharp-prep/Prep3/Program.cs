9130a242e02f205dd9ce6ad93c6fe6fabd3cd44d
ziondehar <ziondehar@gmail.com>
2024-11-09 14:30:55 +0800

    Prog 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {   
+
+        
+        int magicNumber = 63;
+        int guess = 0;
+        while (guess!=magicNumber)
+            {Console.WriteLine("What is the magic number");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess<magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            else if (guess>magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            else 
+                {
+                    Console.WriteLine($"Conratulations you got the Magic Number {magicNumber}!");
+                }
+            }
+    }   
 }
