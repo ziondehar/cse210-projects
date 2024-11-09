9afde3e3368d7465786553176a3eaa579720e6ee
ziondehar <ziondehar@gmail.com>
2024-11-09 12:44:07 +0800

    program for james bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name");
+        string second_name = Console.ReadLine();
+        Console.WriteLine( $"Your name is {second_name} , {first_name} {second_name}");
     }
 }
