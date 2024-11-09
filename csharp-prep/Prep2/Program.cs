8fd3ed54e3d969b1258f80135ba727e467f8a517
ziondehar <ziondehar@gmail.com>
2024-11-09 13:15:52 +0800

    c:\Users\ziond\OneDrive\Desktop\School\cse210\cse210-projects\csharp-prep\Prep2\Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90 && grade <100)
+            {
+                Console.WriteLine("You got an A");
+            }
+        else if (grade >= 80 && grade <100)
+            {
+                Console.WriteLine("You got an B");
+            }
+        else if (grade >= 70 && grade <100)
+            {
+                Console.WriteLine("You got an C");
+            }
+        else if (grade >= 60 && grade <100)
+            {
+                Console.WriteLine("You got an D");
+            }
+        else if (grade < 60 && grade >0)
+            {
+                Console.WriteLine("You got an F");
+            }
+        else 
+            {
+                Console.WriteLine("This grade isnt possible");
+            }
     }
 }
