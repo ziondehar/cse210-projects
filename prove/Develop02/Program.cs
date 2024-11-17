6e39dd664e5861d3ab32efc20ed714bd16d1e032
ziondehar <ziondehar@gmail.com>
2024-11-17 19:57:27 +0800

    Prompt

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+       //Journal
+       
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,4 @@
+using System;
+
+public class PromptGenerator
+    public List<string> _prompts
