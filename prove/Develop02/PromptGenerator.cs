dec4e8b0b77074c81c696d359695ecae22694642
ziondehar <ziondehar@gmail.com>
2024-11-17 20:02:53 +0800

    Updated prompt

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,4 +1,23 @@
 using System;
 
 public class PromptGenerator
-    public List<string> _prompts
+    public List<string> _prompts = new List<string>
+        {
+            "Are you taking enough risks in your life? Would you like to change your relationship to risk? If so how?",
+            "At what point in your life have you had the highest self-esteem?",
+            "Consider and reflect on what might be your 'favorite failure.'",
+            "Draw 25 circles on a page (5x5 grid of circles). Now set a timer for 3 minutes and try to turn each one into something unique. Could be a ball, hand cuffs, a logo, or an eye for instance.",
+            "Draw a small scribble on the page then use your imagination to turn that scribble into a full drawing.",
+            "Find two unrelated objects near you and think of a clever way they might be used together.",
+            "How can you reframe one of your biggest regrets in life?",
+            "How did you bond with one of the best friends you’ve ever had?",
+            "How did your parents or caregivers try to influence or control your behavior when you were growing up?",
+            "How do the opinions of others affect you?",
+            "How do you feel about asking for help?",
+            "How much do your current goals reflect your desires vs someone else’s?"
+        };
+
+        public string GetPrompt()
+            {
+                Random random = newrandom. 
+            }
