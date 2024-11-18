98352fdba5802bf6f2c9f3c17c42b09237d4d59d
ziondehar <ziondehar@gmail.com>
2024-11-18 18:59:46 +0800

    Finished

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,6 +6,7 @@ class Program
     static void Main(string[] args)
     {
        Journal journal = new Journal();
+       PromptGenerator promptGenerator = new PromptGenerator();
 
         while (true)
         {
