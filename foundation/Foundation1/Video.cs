336b640a89d93152d1e0256697901ae29951c2af
ziondehar <ziondehar@gmail.com>
2024-11-28 05:31:10 +0800

    new

---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+{
+    
+}
+public class Video;
+{
+    public string _Title;
+    public string _Author;
+    public int _VideoLength;
+
+    public Video(string _Title, string _Author, int _VideoLength )
+        
+
+}
