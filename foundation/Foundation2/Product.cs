00e592089e76d066e9d68bf714d5c6d85c154008
ziondehar <ziondehar@gmail.com>
2024-11-30 14:21:38 +0800

    products

--- foundation/Foundation2/Product.cs
+++ foundation/Foundation2/Product.cs
@@ -4,12 +4,27 @@ public class Product
 {
    private string _name;
    private string _productId;
-   private int _price;
+   private decimal _price;
    private int _quantity ;
 
-   class Product (string name, string productId, int price, int quantity)
+   public Product (string name, string productId, decimal price, int quantity)
    {
     _name = name;
     _productId = productId;
+    _price = price;
+    _quantity = quantity;
+   }
+   public decimal TotalCost()
+   {
+    return _price * _quantity;
+   }
+
+   public void DisplayProducts()
+   {
+        Console.WriteLine($"Product Name: {_name}");
+        Console.WriteLine($"Product ID: {_productId}");
+        Console.WriteLine($"Price per Unit: ${_price:F2}");
+        Console.WriteLine($"Quantity: {_quantity}");
+        Console.WriteLine($"Total Cost: ${TotalCost():F2}");
    }
 }
