c5c4599a4013c1a98fa7b7e9fcb77e5fa5cd2fe9
ziondehar <ziondehar@gmail.com>
2024-11-30 14:29:05 +0800

    more

--- foundation/Foundation2/Adress.cs
+++ foundation/Foundation2/Adress.cs
@@ -2,5 +2,16 @@ using System;
 
 public class Address
 {
-    
+    private string _StreetAddress;
+    private string _City;
+    private string _Country;
+    private string _State;
+
+    public Address(string streetAddress, string city, string country, string state)
+    {
+         _StreetAddress = streetAddress;
+    _City = city;
+    _Country = country;
+    _State = state;
+    }
 }
