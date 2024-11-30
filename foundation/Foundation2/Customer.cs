0d97fc5630a8237f315feebf8a4a84b620289c0f
ziondehar <ziondehar@gmail.com>
2024-11-30 14:52:40 +0800

    Custom

--- foundation/Foundation2/Customer.cs
+++ foundation/Foundation2/Customer.cs
@@ -3,7 +3,17 @@ using System;
 public class Customer
 {
     private string _name;
-    private string _name;
-    private string _name;
-    private string _name;
+    private Address address;
+
+    public Customer(string)
+    
+    public bool IsInUSA()
+    {
+        return _Country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+    }
+    public string GetFullCustomer()
+    {
+        return $"{_StreetAddress}\n{_City}\n{_State}\n{_Country}";
+    }
 }
+
