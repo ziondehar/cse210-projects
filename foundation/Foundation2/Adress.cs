803e175e8e59d7d135f33fa3678e12affb456db4
ziondehar <ziondehar@gmail.com>
2024-12-02 00:19:19 +0800

    COmplete

--- foundation/Foundation2/Adress.cs
+++ foundation/Foundation2/Adress.cs
@@ -2,12 +2,12 @@ using System;
 
 public class Address
 {
-    private int _StreetAddress;
+    private string _StreetAddress;
     private string _City;
     private string _Country;
     private string _State;
 
-    public Address(int streetAddress, string city, string country, string state)
+    public Address(string streetAddress, string city, string country, string state)
     {
          _StreetAddress = streetAddress;
         _City = city;
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -4,9 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        var address1 = new Address(123, Perth, Australia, WA)
+        var address = new Address("123", "Perth", "Ausralia", "WA");
         Console.WriteLine("Address created:");
-        Console.WriteLine(address1.GetFullAddress());
+        Console.WriteLine(address.GetFullAddress());
         Console.WriteLine();
+
+      
+        var customer = new Customer("BO TURNER", address);
+        Console.WriteLine($"Customer created: {customer.GetName()}");
+        Console.WriteLine($"Lives in USA: {customer.LivesInUSA()}");
+        Console.WriteLine();
+
+        
+        var product1 = new Product("Smartphone", "1010", 699.99m, 1);
+        var product2 = new Product("Headphones", "1011", 49.99m, 2);
+        Console.WriteLine("Products created:");
+        product1.DisplayProducts();
+        Console.WriteLine();
+        product2.DisplayProducts();
+        Console.WriteLine();
+
+        var order = new Order(customer);
+        order.AddProduct(product1);
+        order.AddProduct(product2);
+
+       
+        Console.WriteLine("Order details:");
+        order.DisplayOrder();
+
+      
+        Console.WriteLine($"Total Cost: ${order.CalculateTotalCost():F2}");
     }
+
+
 }
