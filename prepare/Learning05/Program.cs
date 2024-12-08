af2a6531b9305098479d579051496c5431747405
ziondehar <ziondehar@gmail.com>
2024-12-08 20:06:13 +0800

    moving folders

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment mathAssignment = new MathAssignment("Albert Einsteen", "Algebra", "Chapter 5", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Jane Mane", "Creative Writing", "Wassup");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
