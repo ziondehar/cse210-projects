a757454cdc640c997465fabadd13a5fb97836348
ziondehar <ziondehar@gmail.com>
2024-12-12 18:45:58 +0800

    finished

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -8,5 +8,14 @@ class Program
         square.SetColor("RED");
         square.GetArea();
         Console.WriteLine($"Square: Color={square.GetColor()} Area = {square.GetArea()} ");
+    
+        Rectangle rectangle = new Rectangle(4, 12);
+        rectangle.SetColor("Blue");
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        
+        Circle circle = new Circle(18);
+        circle.SetColor("Green");
+        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
     }
-}
+}   
